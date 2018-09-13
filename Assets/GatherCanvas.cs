1604e0223b805035d105a76407491dc629b1d969
Innocenceze <944966330@qq.com>
2018-09-14 00:39:39 +0800

    丰富藏经洞
    。

---
+++ Assets/GatherCanvas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatherCanvas : MonoBehaviour {
+
+    RectTransform RT;
+
+	// Use this for initialization
+	void Start () {
+        RT = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SetBoundary();
+
+    }
+
+    void SetBoundary()
+    {
+        var BG = GameObject.FindGameObjectWithTag("BackGround").GetComponent<SpriteRenderer>().sprite;
+        var width = BG.texture.width / BG.pixelsPerUnit;
+        var height = BG.texture.height / BG.pixelsPerUnit;
+        RT.sizeDelta = new Vector2(width, height);
+    }
+}
--- Assets/Scripts/Controller/GatheringChapter.cs
+++ Assets/Scripts/Controller/GatheringChapter.cs
@@ -6,10 +6,8 @@ using UnityEngine;
 public class GatheringChapter : MonoBehaviour {
 
     [SerializeField]
-    private GameObject bg;
-    [SerializeField]
     private TextMeshProUGUI text;
-
+    public GameObject BG;
 	// Use this for initialization
 	void Start () {
 		
@@ -19,6 +17,22 @@ public class GatheringChapter : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void ChangeBG(string BGname)
+    {
+        var bg = Resources.Load<Sprite>(BGname);
+        BG.GetComponent<SpriteRenderer>().sprite = bg;
+        if (BG.transform.childCount != 0)
+        {
+            int bgcc = BG.transform.childCount;
+            for (int cc = 0; cc < bgcc; cc++)
+            {
+                DestroyImmediate(BG.transform.GetChild(0).gameObject);
+            }
+        }
+        var canvas = Resources.Load<GameObject>(BGname + "canvas");
+        canvas = Instantiate(canvas, BG.transform);
+    }
 
     public void ChangeChapter(int i)
     {
@@ -32,15 +46,15 @@ public class GatheringChapter : MonoBehaviour {
                     "故事讲述了九色鹿（释迦牟尼的前生）救起溺水之人，但溺人见利忘义，到宫廷告密，" +
                     "并带领国王前往猎取鹿角及皮毛，九色鹿毫无惧色，向国王控诉溺人忘恩负义的恶行。国" +
                     "王深受感动，下令全国禁捕九色鹿，而溺人则因违背誓言而全身生疮受苦。";
-                bg.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("九色鹿");
-                    break;
+                ChangeBG("九色鹿");
+                break;
             case 2: text.text = "莫高窟第254窟\n\n" +
                     "开凿于北魏时期（465 - 500年），保存较为完整，有较为完备的佛教图像系统，在艺术成" +
                     "就上也十分突出，同时也拥有较为丰厚的学术研究积淀。\n" +
                     "南壁的舍身饲虎图讲述了释迦牟尼佛前世为Mahasattva王子时，与两位兄长在" +
                     "山间游玩的途中，为了拯救一只因生产而疲惫饥饿以致濒死的母虎和幼虎们，慈悲而决" +
                     "绝地舍出自己肉身饲虎的故事。";
-                bg.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("舍身饲虎");
+                ChangeBG("舍身饲虎");
                 break;
             case 3: text.text = "美术不给图，策划不给文字，我木得办法";break;
         }
